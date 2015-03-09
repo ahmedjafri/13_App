60ff829b94a069fddbd25f8847f96fd6371ce638
Ahmed Jafri <ajafri19@gmail.com>
2015-03-08 17:52:54 -0700

    Drag the card sprite by touch.

--- Classes/ThirteenPlayScene.cpp
+++ Classes/ThirteenPlayScene.cpp
@@ -48,7 +48,9 @@ bool ThirteenPlay::init()
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    auto card = Sprite::create("cards.png");
+    card = Sprite::create("cards.png",Rect(0.0f, 0.0f, 160.0f, 230.0f));
+    card->setPosition(Vec2(origin.x + visibleSize.width/2,
+                           origin.y + visibleSize.height - card->getContentSize().height));
     this->addChild(card, 1);
     
     // create and initialize a label
@@ -61,9 +63,83 @@ bool ThirteenPlay::init()
     // add the label as a child to this layer
     this->addChild(label, 1);
     
+    // listen for touch events
+    auto listener = EventListenerTouchAllAtOnce::create();
+    listener->onTouchesBegan = CC_CALLBACK_2(Layer::onTouchesBegan, this);
+    listener->onTouchesMoved = CC_CALLBACK_2(Layer::onTouchesMoved, this);
+    listener->onTouchesEnded = CC_CALLBACK_2(Layer::onTouchesEnded, this);
+    listener->onTouchesCancelled = CC_CALLBACK_2(Layer::onTouchesEnded, this);
+    this->getEventDispatcher()->
+    addEventListenerWithSceneGraphPriority(listener, this);
+    
     return true;
 }
 
+Point ThirteenPlay::touchToPoint(Touch* touch)
+{
+    // convert the touch object to a position in our cocos2d space
+    return CCDirector::getInstance()->convertToGL(touch->getLocationInView());
+}
+
+bool ThirteenPlay::isTouchingSprite(Touch* touch)
+{
+    // here's one way, but includes the rectangular white space around our sprite
+    //return CGRectContainsPoint(this->sprite->boundingBox(), this->touchToPoint(touch));
+    
+    // this way is more intuitive for the user because it ignores the white space.
+    // it works by calculating the distance between the sprite's center and the touch point,
+    // and seeing if that distance is less than the sprite's radius
+    return (this->card->getPosition().getDistance(this->touchToPoint(touch)) < 100.0f);
+}
+
+void ThirteenPlay::onTouchesBegan(const std::vector<Touch*>& touches, Event* event)
+{
+    // reset touch offset on beginning new touch event
+    this->touchOffset = Point::ZERO;
+    
+    for( auto touch : touches )
+    {
+        // if this touch is within our sprite's boundary
+        if( touch && this->isTouchingSprite(touch) )
+        {
+            // calculate offset from sprite to touch point
+            this->touchOffset = this->card->getPosition() - this->touchToPoint(touch);
+        }
+    }
+}
+
+void ThirteenPlay::onTouchesMoved(const std::vector<Touch*>& touches, Event* event)
+{
+    for( auto touch : touches )
+    {
+        // move the card
+        if( touch && touchOffset.x && touchOffset.y )
+            this->card->setPosition(this->touchToPoint(touch) + this->touchOffset);
+    }
+}
+
+void ThirteenPlay::onTouchesEnded(const std::vector<Touch*>& touches, Event* event)
+{
+    for( auto touch : touches )
+    {
+        if( touch && touchOffset.x && touchOffset.y  )
+        {
+            // move card to final position
+            this->card->setPosition(this->touchToPoint(touch) + this->touchOffset);
+            
+            // stop any existing actions and reset the scale
+            this->card->stopAllActions();
+            this->card->setScale(1.0f);
+            
+            // animate letting go of the sprite
+            this->card->runAction(Sequence::create(
+                                                     ScaleBy::create(0.125f, 1.111f),
+                                                     ScaleBy::create(0.125f, 0.9f),
+                                                     nullptr
+                                                     ));
+        }
+    }
+}
 
 void ThirteenPlay::menuCloseCallback(Ref* pSender)
 {
--- Classes/ThirteenPlayScene.h
+++ Classes/ThirteenPlayScene.h
@@ -3,6 +3,8 @@
 
 #include "cocos2d.h"
 
+using namespace cocos2d;
+
 class ThirteenPlay : public cocos2d::Layer
 {
 public:
@@ -17,6 +19,27 @@ public:
     
     // implement the "static create()" method manually
     CREATE_FUNC(ThirteenPlay);
+    
+    /// Returns the Cocos2d position of the touch
+    Point touchToPoint(Touch* touch);
+    
+    /// Returns true if the touch is within the boundary of our sprite
+    bool isTouchingSprite(Touch* touch);
+    
+    // we are multi-touch enabled, so we must use the ccTouches functions
+    // vs the ccTouch functions
+    void onTouchesBegan(const std::vector<Touch*>& touches, Event* event);
+    void onTouchesMoved(const std::vector<Touch*>& touches, Event* event);
+    void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);
+    
+private:
+    Sprite* card;
+    
+    typedef ThirteenPlay self;
+    typedef Layer super;
+    
+    /// The offset of a touch
+    Point touchOffset;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
